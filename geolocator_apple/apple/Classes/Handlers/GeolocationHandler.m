824aa485c1bb88d02b2075f4ab07b232a8c2409d
Sergey Minakov <naithar@icloud.com>
2021-10-19 14:01:51 +0300

    heading support

--- geolocator_apple/apple/Classes/Handlers/GeolocationHandler.m
+++ geolocator_apple/apple/Classes/Handlers/GeolocationHandler.m
@@ -15,6 +15,9 @@
 @property (strong, nonatomic) GeolocatorError errorHandler;
 @property (strong, nonatomic) GeolocatorResult resultHandler;
 
+@property (strong, nonatomic) CLLocation *latestLocation;
+@property (assign, nonatomic) CLLocationDirection latestCourse;
+
 @end
 
 @implementation GeolocationHandler
@@ -52,19 +55,31 @@
 - (void)startUpdatingLocationWithDesiredAccuracy:(CLLocationAccuracy)desiredAccuracy
                                   distanceFilter:(CLLocationDistance)distanceFilter {
   CLLocationManager *locationManager = self.locationManager;
+
+  self.latestLocation = nil;
+  self.latestCourse = -1;
+
   locationManager.desiredAccuracy = desiredAccuracy;
   locationManager.distanceFilter = distanceFilter;
+  locationManager.headingFilter = 1;
   
 #if TARGET_OS_IOS
   if (@available(iOS 9.0, macOS 11.0, *)) {
-      locationManager.allowsBackgroundLocationUpdates = [GeolocationHandler shouldEnableBackgroundLocationUpdates];
+    locationManager.allowsBackgroundLocationUpdates = [GeolocationHandler shouldEnableBackgroundLocationUpdates];
+
+    if (@available(iOS 11.0, *)) {
+        locationManager.showsBackgroundLocationIndicator = [GeolocationHandler shouldEnableBackgroundLocationUpdates];
+    }
   }
+  locationManager.pausesLocationUpdatesAutomatically = NO;
 #endif
   
   [locationManager startUpdatingLocation];
+  [locationManager startUpdatingHeading];
 }
 
 - (void)stopListening {
+    [self.locationManager stopUpdatingHeading];
     [self.locationManager stopUpdatingLocation];
     
     self.errorHandler = nil;
@@ -79,13 +94,60 @@
     return _locationManager;
 }
 
+- (void)locationManager:(CLLocationManager *)manager 
+       didUpdateHeading:(CLHeading *)newHeading {
+    CLLocationDirection heading = newHeading.trueHeading > 0 ? newHeading.trueHeading : newHeading.magneticHeading;
+    self.latestCourse = heading;
+
+    [self notifyLocationUpdate];
+}
+
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray<CLLocation *> *)locations {
     if (!self.resultHandler) return;
     
     if ([locations lastObject]) {
-        self.resultHandler([locations lastObject]);
+        self.latestLocation = [locations lastObject];
+
+        [self notifyLocationUpdate];
     }
+}
+
+- (void)notifyLocationUpdate {
+    CLLocation *location = [self mappedLocation];
+
+    if (location) {
+        self.resultHandler(location);
+    }
+}
+
+- (CLLocation *)mappedLocation {
+    if (!self.latestLocation) {
+        return nil;
+    }
+
+    CLLocationDirection heading;
+
+    if (self.latestLocation.course > 0) {
+        heading = self.latestLocation.course;
+    } else if (self.latestCourse > 0) {
+        heading = self.latestCourse;
+    } else {
+        heading = -1;
+    }
+
+    CLLocation *location = [[CLLocation alloc] 
+        initWithCoordinate:self.latestLocation.coordinate
+                  altitude:self.latestLocation.altitude
+        horizontalAccuracy:self.latestLocation.horizontalAccuracy
+          verticalAccuracy:self.latestLocation.verticalAccuracy
+                    course:heading
+            courseAccuracy:self.latestLocation.courseAccuracy
+                     speed:self.latestLocation.speed 
+             speedAccuracy:self.latestLocation.speedAccuracy
+                 timestamp:self.latestLocation.timestamp];
+
+    return location;
 }
 
 - (void)locationManager:(CLLocationManager *)manager
